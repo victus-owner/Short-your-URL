6cf9767851c6b10d5cbb144f8a4babf8d4f2e028
victus-owner <ashishprajapati0202@gmail.com>
2026-03-02 15:28:12 +0530

    working server

--- url/app.js
+++ url/app.js
@@ -4,17 +4,24 @@ import path from "path";
 
 const PORT = 3003;
 
+const serveFile = async (res, filePath, contentType) => {
+  try {
+    const data = await readFile(filePath);
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(data);
+  } catch (error) {
+    res.writeHead(404, { "Content-type": contentType });
+    res.end("404 page not found");
+  }
+};
 const server = createServer(async (req, res) => {
   console.log(req.url);
   if (req.method === "GET") {
     if (req.url === "/") {
-      try {
-        const data = await readFile(path.join("public", "index.html"));
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      } catch (error) {
-        res.writeHead(404, { "Content-type": "text/html" });
-        res.end("404 page not found");
+      return serveFile(res, path.join("public", "index.html"), "text/html");
+    } else if (req.method === "GET") {
+      if (req.url === "/style.css") {
+        return serveFile(res, path.join("public", "style.css"), "text/css");
       }
     }
   }
